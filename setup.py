0fa903e89ef9d508bbff5f6fb17fe3ba26962494
u1234x1234 <u1234x1234@gmail.com>
2015-11-17 20:39:48 +0500

    exact version checking

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ setup(
     package_dir={'': '.'},
     packages=find_packages('.'),
     install_requires=[
-        'numpy==1.9.3',
-        'scipy==0.16.0',
-        'bitstring==3.1.3',
-        'deap==1.0.2',
-        'scikit-learn==0.16.1',
+        'numpy>=1.9.3',
+        'scipy>=0.16.0',
+        'bitstring>=3.1.3',
+        'deap>=1.0.2',
+        'scikit-learn>=0.16.1',
     ],
 )
