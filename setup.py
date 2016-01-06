47d7150635ab02f0d60c3fcfdf603b3fa1e0ca00
Rodrigo <test@test.com>
2016-01-05 21:46:36 -0300

    more fixes for pip

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sklearn-deap',
-    version='0.1.1',
+    version='0.1.2',
     author='Rodrigo',
     author_email='',
     description='Use evolutionary algorithms instead of gridsearch in scikit-learn.',
     url='https://github.com/rsteca/sklearn-deap',
-    download_url='https://github.com/rsteca/sklearn-deap/tarball/0.1.1',
+    download_url='https://github.com/rsteca/sklearn-deap/tarball/0.1.2',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 2',
@@ -22,7 +22,8 @@ setup(
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
     ],
-    packages=['sklearn-deap'],
+    package_dir={'': '.'},
+    packages=find_packages('.'),
     install_requires=[
         'numpy>=1.9.3',
         'scipy>=0.16.0',
