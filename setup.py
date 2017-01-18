28376f87a77cb0681f580a47ac5d502d395feac2
Rodrigo <test@test.com>
2017-01-17 21:01:16 -0300

    version bump

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='sklearn-deap',
-    version='0.1.7',
+    version='0.1.8',
     author='Rodrigo',
     author_email='',
     description='Use evolutionary algorithms instead of gridsearch in scikit-learn.',
     url='https://github.com/rsteca/sklearn-deap',
-    download_url='https://github.com/rsteca/sklearn-deap/tarball/0.1.7',
+    download_url='https://github.com/rsteca/sklearn-deap/tarball/0.1.8',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 2',
