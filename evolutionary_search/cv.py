e5a46da8d07a7974fed55c70d423d1f22b7dc6ca
Ryan Peach <ryanpeach@users.noreply.github.com>
2017-07-18 14:31:41 -0500

    Issue27
    Try this to get the first evaluation, also added the selection to capture all available data.

--- evolutionary_search/cv.py
+++ evolutionary_search/cv.py
@@ -409,8 +409,10 @@ class EvolutionaryAlgorithmSearchCV(BaseSearchCV):
 
         # History
         hist = tools.History()
+        toolbox.decorate("evaluate", hist.decorator)
         toolbox.decorate("mate", hist.decorator)
         toolbox.decorate("mutate", hist.decorator)
+        toolbox.decorate("select", hist.decorator)
         hist.update(pop)
 
         if self.verbose:
